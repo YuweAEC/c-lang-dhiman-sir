e95a838fd27097cd7c8aa60982e86ab385e5e6b0
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-14 23:51:29 +0530

    add range to scanf

--- Day 2/by sir/scan.c
+++ Day 2/by sir/scan.c
@@ -3,7 +3,7 @@ int main()
 {
      int a,b,c,x;
      printf("Enter three numbers ");
-	x=scanf("%d%d%d",&a,&b,&c);
+	x=scanf("%3d%3d%2d",&a,&b,&c);
      printf("a=%d  b=%d  c=%d    x=%d\n",a,b,c,x);
      return 0;
 }
