436403019263e037f653f36dcd4c8a914fc708ba
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-14 23:24:52 +0530

    add new scan file

---
+++ Day 2/scan.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+     int a,b,c,x;
+     printf("Enter three numbers ");
+	x=scanf("%d%d%d",&a,&b,&c);
+     printf("a=%d  b=%d  c=%d    x=%d\n",a,b,c,x);
+     return 0;
+}
