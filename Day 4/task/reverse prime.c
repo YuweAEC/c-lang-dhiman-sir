add2e694b05b0721a84cabc7d80bc0ef1cfc8dab
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-15 23:48:00 +0530

    add new to file

--- Day 4/task/reverse prime.c
+++ Day 4/task/reverse prime.c
@@ -9,4 +9,29 @@ int main()
     printf("Enter a Number:-");
     scanf("%d", &y);
     
+    if (y<0)
+    {
+        printf("Negative NO. cannot be prime no");
+        exit(0);
+    }
+
+    for (y1=2 ; y1 < y/2; y1++)
+    {
+        y1=isPrime(y1);
+
+        if (s1==1)
+        {
+            y2=y-y1;
+            s2=isPrime(y2);
+
+            if (s2==1)
+            {
+                printf("%d %d \n", y1, y2);
+            }
+            
+        }
+        
+    }
+    
+    
 }
