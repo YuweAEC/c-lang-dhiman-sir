91a8f505c834f5d0df65f63b7c0331a8418edfd6
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-16 09:27:45 +0530

    add enw file into my repo

---
+++ Day 5/String input using dma.c
@@ -0,0 +1,14 @@
+int main()
+{
+    char *str,c;
+    int j=1;
+    str=(char*)malloc(sizeof(char));
+    printf("ENter string ;");
+    do
+    {
+        c=getchar();
+        
+    } 
+    while (c!=='\n');
+    
+}
